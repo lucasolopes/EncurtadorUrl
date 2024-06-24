aa55e00768025893b2fe62ab2ea2df73b6977fde
lucasolopes <olopes.lucas567@gmail.com>
2024-06-24 15:25:12 -0300

    Criando Entidade encurtador

---
+++ EncurtadorUrl/Models/Entities/Encurtador.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EncurtadorUrl.Models.Entities
+{
+    public class Encurtador
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        public string link { get; set; }
+
+        [Required]
+        public string shortUrl { get; set; }
+
+        [Required]
+        public DateTime dateTime { get; set; }
+
+
+    }
+}
