5b72d1fa6b91a1de7b6236e33a5f2e0e9164b1d0
lucasolopes <olopes.lucas567@gmail.com>
2024-06-24 15:25:52 -0300

    criando repository do encurtador

---
+++ EncurtadorUrl/Repositories/Implementations/EncurtadorRepository.cs
@@ -0,0 +1,32 @@
+﻿using EncurtadorUrl.Data.Context;
+using EncurtadorUrl.Repositories.Interfaces;
+
+namespace EncurtadorUrl.Repositories.Implementations
+{
+    public class EncurtadorRepository : IEncurtadorRepository
+    {
+        private readonly EncurtadorContext _context;
+
+
+        public EncurtadorRepository(EncurtadorContext context)
+        {
+            _context = context;
+        }
+        
+        public void AddShorLink(string link, string shortLink, DateTime dateTime)
+        {
+            _context.Add(new EncurtadorUrl.Models.Entities.Encurtador
+            {
+                link = link,
+                shortUrl = shortLink,
+                dateTime = dateTime
+            });
+            _context.SaveChanges();
+        }
+
+        public List<string> GetAll()
+        {
+            return _context.Encurtadors.Select(e => e.shortUrl).ToList();
+        }
+    }
+}
---
+++ EncurtadorUrl/Repositories/Interfaces/IEncurtadorRepository.cs
@@ -0,0 +1,9 @@
+﻿
+namespace EncurtadorUrl.Repositories.Interfaces
+{
+    public interface IEncurtadorRepository
+    {
+        void AddShorLink(string link, string shortLink, DateTime dateTime);
+        List<string> GetAll();
+    }
+}
