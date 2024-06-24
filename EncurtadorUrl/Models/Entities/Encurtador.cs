731a19ca2bf0687909bb4a144357c9859474271f
lucasolopes <olopes.lucas567@gmail.com>
2024-06-24 15:46:46 -0300

    Adiciando data de Expiracao na tabela encurtador

--- EncurtadorUrl/Models/Entities/Encurtador.cs
+++ EncurtadorUrl/Models/Entities/Encurtador.cs
@@ -17,6 +17,9 @@ namespace EncurtadorUrl.Models.Entities
         [Required]
         public DateTime dateTime { get; set; }
 
+        [Required]
+        public DateTime DataExpiracao { get; set; }
+
 
     }
 }
