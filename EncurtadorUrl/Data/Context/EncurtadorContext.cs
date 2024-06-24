7fc0ebaeb1b82b399a332ca6969fbe2df71d2217
lucasolopes <olopes.lucas567@gmail.com>
2024-06-24 15:25:32 -0300

    criando context do banco

---
+++ EncurtadorUrl/Data/Context/EncurtadorContext.cs
@@ -0,0 +1,12 @@
+﻿using EncurtadorUrl.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EncurtadorUrl.Data.Context
+{
+    public class EncurtadorContext : DbContext
+    {
+        public EncurtadorContext(DbContextOptions<EncurtadorContext> options) : base(options) { }
+
+        public DbSet<Encurtador> Encurtadors { get; set; }
+    }
+}
