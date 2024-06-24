f4967512f58fb818b10e37dadfc315d921898e0f
lucasolopes <olopes.lucas567@gmail.com>
2024-06-24 15:27:18 -0300

    adicinando configuracoes de abstracao

--- EncurtadorUrl/Program.cs
+++ EncurtadorUrl/Program.cs
@@ -1,5 +1,9 @@
+using EncurtadorUrl.Data.Context;
+using EncurtadorUrl.Repositories.Implementations;
+using EncurtadorUrl.Repositories.Interfaces;
 using EncurtadorUrl.Services.Implementations;
 using EncurtadorUrl.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +13,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<EncurtadorContext>(options =>
+{
+    options.UseInMemoryDatabase("EncurtadorDatabase"); // Nome do banco de dados em memória
+});
+
+builder.Services.AddScoped<IEncurtadorService, EncurtadorService>();
+builder.Services.AddScoped<IEncurtadorRepository, EncurtadorRepository>();
+
 
 var app = builder.Build();
 
