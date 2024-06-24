5f638bba950e438d5cbac55f15f230fe9b0ac7a2
lucasolopes <olopes.lucas567@gmail.com>
2024-06-24 15:27:06 -0300

    criando controler e implementando link encurtado e getall

---
+++ EncurtadorUrl/Controllers/EncurtadorController.cs
@@ -0,0 +1,34 @@
+﻿using EncurtadorUrl.Services.Implementations;
+using EncurtadorUrl.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EncurtadorUrl.Controllers
+{
+    [ApiController]
+    [Route("shorten-url")]
+    public class EncurtadorController : ControllerBase
+    {
+        private readonly IEncurtadorService _encurtadorService;
+
+        public EncurtadorController(IEncurtadorService encurtadorService)
+        {
+            _encurtadorService = encurtadorService;
+        }
+
+        [HttpPost]
+        public ActionResult<string> CreateShortLink(string link)
+        {
+              string shortLink = _encurtadorService.CreateShortLink(link);
+              var request = HttpContext.Request;
+              var fullUrl = $"{request.Scheme}://{request.Host}";
+              return Ok(fullUrl+"//"+shortLink);
+        }
+
+        [HttpGet]
+        public ActionResult<List<string>> GetAllLink()
+        {
+            return Ok(_encurtadorService.GetShortLinks());
+        }
+
+    }
+}
